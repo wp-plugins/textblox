6f3168ad59dc1ae7fcd849736781c6bc849b677e
PowieT <PowieT@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-13 10:37:07 +0000

    About Box Fix
    git-svn-id: https://plugins.svn.wordpress.org/textblox/tags/0.9.3@651912 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- textblox_settings.php
+++ textblox_settings.php
@@ -13,14 +13,20 @@
 
 <div class="postbox">
 <h3><?php _e('About', 'textblox') ?></h3>
-	<div class="inside">
+	<div class="inside" style="overflow:auto">
+		<div style="float:left;margin-right: 10px; display:inline;">
 		<!-- www -->
 		WWW: <a href="http://www.powie.de">powie.de</a>
+		</div>
+		<div style="float:left;margin-right: 10px; display:inline;">
 		<!-- twitter -->
 		<a href="https://twitter.com/PowieT" class="twitter-follow-button" data-show-count="false" data-lang="de">@PowieT folgen</a>
 		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		</div>
+		<div style="float:left;margin-right: 10px; display:inline;">
 		<!-- fb -->
-		<iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpowiede&amp;send=false&amp;layout=standard&amp;width=450&amp;show_faces=false&amp;font&amp;colorscheme=light&amp;action=like&amp;height=35" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:35px;" allowTransparency="true"></iframe>
+		<iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpowiede&amp;send=false&amp;layout=standard&amp;width=450&amp;show_faces=false&amp;font&amp;colorscheme=light&amp;action=like&amp;height=35" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:350px; height:35px;" allowTransparency="true"></iframe>
+		</div>
     </div>
 </div>
 
