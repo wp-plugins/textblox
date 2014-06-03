8a294485c39be98d06ea91909b9f5589ac6d1c2e
PowieT <PowieT@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-03 07:11:25 +0000

    Format Bug Fix
    git-svn-id: https://plugins.svn.wordpress.org/textblox/tags/0.9.4@925538 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,61 @@
+=== Powie's TextBlox ===
+Contributors: PowieT
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=UST438WYW6HNJ
+Tags: Text, Blocks, Templates, Area, Section, Content
+Requires at least: 2.5
+Tested up to: 3.8
+Stable tag: 0.9.4
+License: GPLv2
+
+Create Textblocks and insert it into your pages using a shortcode. You can very simple update standard text
+
+== Description ==
+Create textblocks, areas, sections or other default content and insert it into your pages using a shortcode. So you can simple update standard text. Ultimate Plugin if you have always same content on many posts or pages. Simple update the TextBlox post, and all content is changed on the pages that includes that TextBlox content.
+
+To insert a TextBlox content on a page or post include Shortcode: [textblox id=123]. You can Pick the right content with the Picker in TinyMCE, where all TextBlox are listed.
+
+= Features: =
+creates a simple custom post style
+tinymce button to seelect content for simple shortcode creation
+
+= Documentation =
+**Basic example:**
+
+[textblox id="123"] where 123 is the ID of the TextBlox Post ID.
+This shortcode will show the text inside your page
+
+= Support =
+Support Forum @ [powie.de](http://goo.gl/lfR7B)
+
+== Installation ==
+
+1. Upload `textblox` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Place the shortcode on your page or `<?php echo do_shortcode('[textblox id=123]'); ?>` in your templates
+
+== Frequently Asked Questions ==
+
+Use our forum at www.powie.de for support
+
+== Changelog ==
+= 0.9.4 (03.06.2014) =
+* Format Bug Fixed
+
+= 0.9.3 (08.01.2013) =
+* Localisation
+* de_DE Language
+
+= 0.9.2 =
+* New settings page
+
+= 0.9.1 =
+* TinyMCE button
+
+= 0.9.0 =
+* initial version
+
+== Upgrade Notice ==
+na
+
+== Screenshots ==
+na
---
+++ textblox_settings.php
@@ -0,0 +1,46 @@
+<div class="wrap">
+<div class="icon32" id="icon-options-general"></div>
+<h2><?php _e('Textblox Settings', 'textblox') ?></h2>
+<div id="poststuff">
+
+<div class="postbox">
+<h3><?php _e('Usage', 'textblox') ?></h3>
+	<div class="inside">
+	<?php _e('Use shortcode <code>[textblox id=123]</code> to insert your blocks into a page or post.', 'textblox') ?><br />
+	<?php _e('Or pick your content directly in TinyMCE on that icon:', 'textblox') ?> <img src="<?php echo TB_PLUGIN_URL; ?>textblox_16.png" alt="tb" title="TextBlox" />
+    </div>
+</div>
+
+<div class="postbox">
+<h3><?php _e('About', 'textblox') ?></h3>
+	<div class="inside" style="overflow:auto">
+		<div style="float:left;margin-right: 10px; display:inline;">
+		<!-- www -->
+		WWW: <a href="http://www.powie.de">powie.de</a>
+		</div>
+		<div style="float:left;margin-right: 10px; display:inline;">
+		<!-- twitter -->
+		<a href="https://twitter.com/PowieT" class="twitter-follow-button" data-show-count="false" data-lang="de">@PowieT folgen</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		</div>
+		<div style="float:left;margin-right: 10px; display:inline;">
+		<!-- fb -->
+		<iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpowiede&amp;send=false&amp;layout=standard&amp;width=450&amp;show_faces=false&amp;font&amp;colorscheme=light&amp;action=like&amp;height=35" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:350px; height:35px;" allowTransparency="true"></iframe>
+		</div>
+		<div style="float:left;margin-right: 10px; display:inline;">
+			<div class="g-plusone" data-size="small" data-href="http://www.powie.de"></div>
+			<script type="text/javascript">
+			  (function() {
+			    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+			    po.src = 'https://apis.google.com/js/plusone.js';
+			    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+			  })();
+			</script>
+		</div>
+
+    </div>
+</div>
+
+</div>
+
+</div>
